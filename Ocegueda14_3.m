f7801ac1dda94b89b91119c2a55a4a8d3c71c99e
Jason <0933002@my.scccd.edu>
2025-11-10 22:07:20 +0000

    Lab 14_3

--- Ocegueda14_3.m
+++ Ocegueda14_3.m
@@ -2,7 +2,7 @@
 p = input("Enter the coefficients of a cubic polynomial, as a matrix[..]\n");
 y0 = input("Enter the integration constant for that function.\n");
 pDer = polyder(p);
-pint = polyint(p);
+pint = polyint(p,y0);
 % Report the output
 fprintf("The derivative of the function is %gx^2+%gx+%g.\n",pDer)
 fprintf("The integral of the function is %gx^4+%gx^3+%gx^2+%gx+%g.\n",pint)
